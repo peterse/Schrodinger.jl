c09f497985bb03dd34e7a7014027510c4a35a82d
Jeremy <jeremy.bejanin@gmail.com>
2019-04-09 17:55:38 -0400

    export eigvals

--- src/Schrodinger.jl
+++ src/Schrodinger.jl
@@ -25,7 +25,7 @@ import Base: BitSet, print_array, sum
 import LinearAlgebra: adjoint, exp, exp!, tr, eigen
 import Arpack: eigs
 import SpecialFunctions: erf
-export eigs, trace, normalize, normalize!
+export eigs, eigvals, trace, normalize, normalize!
 trace(A::AbstractMatrix) = tr(A)
 sum(A::AbstractArray,i::Integer) = Base._sum(A,i)
 parseb2(s::AbstractString) = Base.tryparse_internal(Int,s,firstindex(s),lastindex(s),2,true)
